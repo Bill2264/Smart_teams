3dd7d9580dca89cafe86fd0b8c469a9f5012df1f
nandita <g00463584@atu.ie>
2025-11-10 10:43:44 +0000

    using strings for categories of budget tracker

---
+++ budget _tracker.py
@@ -0,0 +1,38 @@
+
+from flask import Flask 
+
+import numpy as np    # Using strings to see if words and description work 
+msg= "Budget tracker"
+print(msg)
+
+
+txt=f"ID"
+txt=f"Date"
+txt=f"Item"
+txt=f"Notes"
+txt=f"Type" #still not able to run code
+
+print(txt)
+
+print("Enter the ID no. :")
+ID = input()
+print(f"{id}")
+
+print("Enter date:")
+date = input()
+print(f"{date}")
+
+print("Enter the item:")
+item = input()
+print(f"{item}")
+
+print("Enter the notes:")
+note = input()
+print(f"{note}")
+
+
+
+
+
+
+
---
+++ budget tracker.py
@@ -0,0 +1,60 @@
+#Notice how Python does not use semicolon after a print function
+#Indentation is important
+"""
+import numpy as np
+msg= "Roll a dice!"
+print(msg)
+
+print(np.random.randint(1,9))
+"""
+"""
+
+x= y= z= "Orange"
+print(x)
+print(y)
+print(z)
+
+x, y, z= "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+"""
+"""
+Fruits in Arrays/Collections
+
+fruits=["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+"""
+#x="Python is awesome"
+#print(x)
+
+"""
+x="Python"
+y="is"
+z="awesome"
+print(x+ y+ z) #no white spaces
+
+
+x=5
+y=239
+print(x+y)
+
+x="awesome"
+
+
+def myfunc():
+    print("Python is " +x) #There is white space here
+
+myfunc()
+"""
+x= 1 #int
+y= 2.8 #float
+z= 1j #complex
+
+print(type(x))
+print(type(y))
+print(type(z))
