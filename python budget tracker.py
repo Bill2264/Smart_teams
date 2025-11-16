78d3454275623ce66ba288a3f0c507f82deba7d8
Nandita <g00463584@atu.ie>
2025-11-16 23:38:15 +0000

    inital stages of the layout of the tracker

---
+++ python budget tracker.py
@@ -0,0 +1,15 @@
+class Tracker:
+def __init__(self, date, description, amount): #constructor
+self.date= date
+self.description= description
+self.amount= amount 
+
+
+#Initial attempt at making the very basic layout of the budget tracker
+print("Expense Tracker Menu:")
+print("1. Add Expense")
+print("2. Remove Expense")
+print("3. View Expense")
+print("4. Total Expense")
+
+print("5. Exit")
