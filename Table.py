4704ea5e0ec1b10481d6050ed0d3a4494dcc33b5
Billy Lis-Murphy <G00465240@atu.ie>
2025-11-17 10:54:00 +0000

    work in progress table

--- Table.py
+++ Table.py
@@ -4,5 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<!DOCTYPE html><html><head><title>Flask Dynamic Data</title></head><body><h1>Table</h1><div></div><table><tr><th>Date</th><th>Amount</th><th>type</th><th>Note</th></tr><tr><td>11/11/11</td><td>$100</td><td>Income</td><td>car wash</td></tr></table> </body></html>"
+   return render_template('index.html')
+
+
+@app.route("/")
+def end_table():
+    return render_template('end_table.html')
+
+@app.route("/")
+def end():
+    return render_template('end.html')
+
 
