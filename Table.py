da063a1594f72c20b11721794c090952f27d2e6b
Billy Lis-Murphy <G00465240@atu.ie>
2025-11-17 09:50:56 +0000

    Display table

--- Table.py
+++ Table.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    render_template('index.html')
+    return "<!DOCTYPE html><html><head><title>Flask Dynamic Data</title></head><body><h1>Table</h1><div></div><table><tr><th>Date</th><th>Amount</th><th>type</th><th>Note</th></tr><tr><td>11/11/11</td><td>$100</td><td>Income</td><td>car wash</td></tr></table> </body></html>"
 
