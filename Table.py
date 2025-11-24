ef139087ff3d0247e90ed84592a772d6c68de2c2
Billy Lis-Murphy <G00465240@atu.ie>
2025-11-24 10:50:57 +0000

    stuff

--- Table.py
+++ Table.py
@@ -1,19 +1,21 @@
 from flask import Flask, render_template, request, jsonify
-from turbo_flask import Turbo
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-turbo = Turbo(app)
 
 @app.route("/")
 def index():
     return render_template('index.html')
     
-@app.route("/update")
+@app.route("/update", methods=['row'])
 def row():
-    return turbo.stream(turbo.replace('dynamic-content', 'Updated Content!'))
+    return render_template('table.html')
 
 class data:
     date=11/11/11
     amount = 100.0
     type = 'income'
     note = 'default'
+
+if __name__ == '__main__':
+    app.run(debug=True)
