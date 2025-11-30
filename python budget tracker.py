be2e0358b848f9c6221a9d88b1fdfa5b8582427a
Nandita <g00463584@atu.ie>
2025-11-30 18:06:25 +0000

    added loop to the menu

--- python budget tracker.py
+++ python budget tracker.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 from tkinter import ttk, messagebox, simpledialog
-from flask import flask
+
 
 #Class with budget tracker
-
 
 class ExpenseTracker:
     def __init__(self, date, description, amount):
@@ -29,35 +28,37 @@ def add_expense(expenses):
     expense = ExpenseTracker(date, description, amount)
     expenses.append(expense)
     
+expenses = []  # must be BEFORE the loop
 
+while True:
+    # Initial attempt at making the very basic layout of the budget tracker
+    print("Expense Tracker Menu:")
+    print("1. Add Expense")
+    print("2. Remove Expense")
+    print("3. View Expense")
+    print("4. Total Expense")
+    print("5. Exit")
 
-#Initial attempt at making the very basic layout of the budget tracker
-print("Expense Tracker Menu:")
-print("1. Add Expense")
-print("2. Remove Expense")
-print("3. View Expense")
-print("4. Total Expense")
-print("5. Exit")
-choice = input("Enter your choice (1-6): ")
-expenses = []
-if choice == '1':
-    add_expense(expenses)
+    choice = input("Enter your choice (1-6): ")
 
-if choice == '2':
-    remove_expense(expenses)
+    if choice == '1':
+        add_expense(expenses)
 
-if choice == '3':
-    view_expenses(expenses)
-    
-if choice == '4':
-    total_expense(expenses)
+    if choice == '2':
+        remove_expense(expenses)
 
-if choice == '5':
-    edit_expense(expenses)
+    if choice == '3':
+        view_expenses(expenses)
 
-if choice == '6':
-    print("Exited Successfully")
+    if choice == '4':
+        total_expense(expenses)
 
+    if choice == '5':
+        edit_expense(expenses)
+
+    if choice == '6':
+        print("Exited Successfully")
+        break
 
 def view_expenses(expenses):
     if not expenses:
